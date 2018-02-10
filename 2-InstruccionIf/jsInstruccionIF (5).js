da2b7800cb4c2e83ce2119414bfaa314995deb52
julianfusco <36314161+julianfusco@users.noreply.github.com>
2018-02-09 22:09:00 -0300

    ejercicio 5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,16 @@
+//Al ingresar una edad solo debemos informar si la persona NO es adolescente.//
+
 function Mostrar()
 {
-//tomo la edad  
+	var edad;
 
+	edad = document.getElementById('edad').value;
+//tomo la edad  
+if (edad >= 18 || edad <= 13)
+
+{
+
+	alert("la persona no es adolescente");
+}
 
 }//FIN DE LA FUNCIÓN
