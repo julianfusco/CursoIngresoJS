0e096bd46911aaf565c6d23310ddf74765ffc1be
julianfusco <36314161+julianfusco@users.noreply.github.com>
2018-02-09 21:58:19 -0300

    ejercicio 4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
 //tomo la edad  
+ 
+ if (edad >= 13 && edad <= 17)
+
+ 	
+{
+
+	alert("la persona es adolescente");
+}
 
 
 
