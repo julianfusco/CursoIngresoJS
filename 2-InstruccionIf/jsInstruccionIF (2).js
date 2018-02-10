2c3ac2e7b9de4073a465385e785727dc82791b8d
julianfusco <36314161+julianfusco@users.noreply.github.com>
2018-02-09 21:35:31 -0300

    ejercicio 2

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
+	var edad;
 //tomo la edad  
+ edad = document.getElementById('edad').value;
 
+if (edad >= 18)
+
+alert("la persona es mayor de edad")
 
 
 }//FIN DE LA FUNCIÓN
